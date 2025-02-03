64a991038c78b49a0e4879bc2e97b725aee2e7a9
George Devine <george@photocrowd.com>
2025-02-03 18:18:07 +0000

    Fixed path for QuadrilateralAnalyzer.tsx

--- components/QuadrilateralAnalyzer.tsx
+++ components/QuadrilateralAnalyzer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 
 const QuadrilateralAnalyzer = () => {
   const GRID_SIZE = 20;
