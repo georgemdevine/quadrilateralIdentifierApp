065d19e82533ed58c2a01277724452d54dfd483e
George Devine <george@photocrowd.com>
2025-02-03 17:30:15 +0000

    Adding deployment functionality.

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import { QuadrilateralAnalyzer } from '../components/QuadrilateralAnalyzer'
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <QuadrilateralAnalyzer />
+        </main>
+    )
+}
